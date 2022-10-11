d4e45f00a99cf6ed20989e890fcf9d447df519fe
Kevin Hölscher <kevin-hoelscher@gmx.de>
2022-10-11 04:36:14 +0200

    added more tests with error handlings

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting.js";
 
 describe("Greeting component", () => {
@@ -12,5 +13,42 @@ describe("Greeting component", () => {
     // Assert
     const helloWorldElement = screen.getByText("Hello World!");
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders It`s good to see you! when the button is not clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Assert
+    const goodToSeeYouElement = screen.getByText("It`s good to see you!");
+    expect(goodToSeeYouElement).toBeInTheDocument();
+  });
+
+  test("renders changed! if the button was clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.getByText("Changed!", { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("does not render if the button was clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.queryByText("It`s good to see you!", {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
   });
 });
